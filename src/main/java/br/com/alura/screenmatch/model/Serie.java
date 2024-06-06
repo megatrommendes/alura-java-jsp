c3145c3510b309602fe336fcb3ca527be3f513ab
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2024-06-06 11:52:48 -0300

    adicionando api de tradução

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import br.com.alura.screenmatch.service.ConsultaLibreTranslate;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -34,7 +35,7 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+        this.sinopse = ConsultaLibreTranslate.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
     public Long getId() {
